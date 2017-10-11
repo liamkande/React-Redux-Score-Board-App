9831f0f129536d6855893939ac4dcfa2efebe91c
Ansoumane Kande <liam-kande@Ansoumanes-MBP.home>
2017-10-11 06:05:03 -0400

    corrected class name

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,7 +5,7 @@ export const Counter = (props) => (
 <div className="counter">
   <button className="counter-action decrement"> - </button>
   <div className="counter-score"> {props.score} </div>
-  <button className="counter-action decrement"> + </button>
+  <button className="counter-action increment"> + </button>
 </div>
 )
 
