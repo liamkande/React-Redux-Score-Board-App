b1fe8ffec05f55331477171dabfa14330fba3536
Ansoumane Kande <liam-kande@Ansoumanes-MBP.home>
2017-10-16 17:48:42 -0400

    Added new PlayerDetail component and managed State.

--- src/containers/ScoreBoard.js
+++ src/containers/ScoreBoard.js
@@ -3,21 +3,27 @@ import PropTypes from 'prop-types'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import * as PlayerActionCreators from '../actions/player'
-import Player from '../components/Player'
 import Header from '../components/Header'
+import Player from '../components/Player'
 import AddPlayerForm from '../components/AddPlayerForm'
+import PlayerDetail from '../components/PlayerDetail'
 
 class Scoreboard extends Component {
-
   static propTypes = {
     players: PropTypes.array.isRequired
   }
 
   render() {
-    const { dispatch, players } = this.props;
-    const addPlayer = bindActionCreators(PlayerActionCreators.addPlayer, dispatch);
-    const removePlayer = bindActionCreators(PlayerActionCreators.removePlayer, dispatch);
+    const { dispatch, players, selectedPlayerIndex } = this.props
+    const addPlayer = bindActionCreators(PlayerActionCreators.addPlayer, dispatch)
+    const removePlayer = bindActionCreators(PlayerActionCreators.removePlayer, dispatch)
     const updatePlayerScore = bindActionCreators(PlayerActionCreators.updatePlayerScore, dispatch)
+    const selectPlayer = bindActionCreators(PlayerActionCreators.selectPlayer, dispatch)
+
+    let selectedPlayer;
+    if(selectedPlayerIndex !== -1){
+      selectedPlayer = players[selectedPlayerIndex];
+    }
 
     const playerComponents = players.map((player, index) => (
       <Player
@@ -27,9 +33,9 @@ class Scoreboard extends Component {
         key={player.name}
         updatePlayerScore={updatePlayerScore}
         removePlayer={removePlayer}
+        selectPlayer={selectPlayer}
       />
-    ))
-
+    ));
     return (
       <div className="scoreboard">
         <Header players={players} />
@@ -37,15 +43,19 @@ class Scoreboard extends Component {
           { playerComponents }
         </div>
         <AddPlayerForm addPlayer={addPlayer} />
+        <div className="player-detail">
+          <PlayerDetail selectedPlayer={selectedPlayer} />
+        </div>
       </div>
-    )
+    );
   }
 }
 
 const mapStateToProps = state => (
   {
-    players: state
+    players: state.players,
+    selectedPlayerIndex: state.selectedPlayerIndex
   }
-)
+);
 
 export default connect(mapStateToProps)(Scoreboard)
--- src/reducers/player.js
+++ src/reducers/player.js
@@ -1,47 +1,80 @@
 import * as PlayerActionTypes from '../actiontypes/player'
 
-const initialState = [
-  {
+const initialState = {
+  players: [{
     name: 'Liam Kande',
-    score: 17
+    score: 17,
+    created: '10/16/2017',
+		updated: '10/25/2017'
   },
   {
     name: 'Dimitri Luzgin',
-    score: 15
+    score: 15,
+    created: '10/16/2017',
+    updated: '10/23/2017'
   },
   {
     name: 'Levick Mboukou',
-    score: 12
+    score: 12,
+    created: '10/16/2017',
+    updated: '10/20/2017'
   }
-]
-
+],
+selectedPlayerIndex: -1
+}
 export default function Player(state=initialState, action) {
-  switch(action.type) {
-    case PlayerActionTypes.ADD_PLAYER:
-      return [
-        ...state,
-        {
+
+	let date = new Date()
+	let day = date.getDate()
+	let month = date.getMonth()+1
+	let year = date.getFullYear()
+
+  switch(action.type){
+    case PlayerActionTypes.ADD_PLAYER: {
+			const addPlayerList = [...state.players, {
           name: action.name,
-          score: 0
-        }
-      ];
+          score: 0,
+          created: `${month}/${day}/${year}`
+      }];
+      return {
+        ...state,
+				players: addPlayerList
+		 	};
+	 	}
 
-    case PlayerActionTypes.REMOVE_PLAYER:
-      return [
-        ...state.slice(0, action.index),
-        ...state.slice(action.index + 1)
-      ];
+    case PlayerActionTypes.REMOVE_PLAYER: {
+			const removePlayerList = [
+				...state.players.slice(0, action.index),
+				...state.players.slice(action.index + 1)
+			];
+      return {
+				...state,
+				players: removePlayerList
+			}
+		}
 
-    case PlayerActionTypes.UPDATE_PLAYER_SCORE:
-      return state.map((player, index) => {
-        if(index === action.index) {
+    case PlayerActionTypes.UPDATE_PLAYER_SCORE: {
+			const updatePlayerList = state.players.map((player, index) => {
+        if(index === action.index){
           return {
             ...player,
-            score: player.score + action.score
-          }
+             score: player.score + action.score,
+             updated: `${month}/${day}/${year}`
+           };
         }
         return player
       });
+			return {
+				...state,
+				players: updatePlayerList
+			};
+		}
+
+	  case PlayerActionTypes.SELECT_PLAYER:
+		  return {
+				...state,
+				selectedPlayerIndex: action.index
+			};
 
     default:
       return state
